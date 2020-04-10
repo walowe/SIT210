f81a860be9697976c1cafcf027b84a589448cab3
walowe <63434883+walowe@users.noreply.github.com>
2020-04-10 12:04:10 +1000

    Add files via upload

---
+++ morseblink.ino
@@ -0,0 +1,112 @@
+int led1 = D7;
+
+void setup() {
+pinMode(led1, OUTPUT);
+
+}
+
+void loop() {
+    
+// W - short long long
+digitalWrite(led1, HIGH);
+delay(250);
+digitalWrite(led1, LOW);
+delay(250);
+
+digitalWrite(led1, HIGH);
+delay(500);
+digitalWrite(led1, LOW);
+delay(250);
+
+digitalWrite(led1, HIGH);
+delay(500);
+digitalWrite(led1, LOW);
+delay(250);
+
+//I - short short
+digitalWrite(led1, HIGH);
+delay(250);
+digitalWrite(led1, LOW);
+delay(250);
+
+digitalWrite(led1, HIGH);
+delay(250);
+digitalWrite(led1, LOW);
+delay(250);
+
+//L - short long short short
+
+digitalWrite(led1, HIGH);
+delay(250);
+digitalWrite(led1, LOW);
+delay(250);
+
+digitalWrite(led1, HIGH);
+delay(500);
+digitalWrite(led1, LOW);
+delay(250);
+
+digitalWrite(led1, HIGH);
+delay(250);
+digitalWrite(led1, LOW);
+delay(250);
+
+digitalWrite(led1, HIGH);
+delay(250);
+digitalWrite(led1, LOW);
+delay(250);
+
+//L - short long short short
+digitalWrite(led1, HIGH);
+delay(250);
+digitalWrite(led1, LOW);
+delay(250);
+
+digitalWrite(led1, HIGH);
+delay(500);
+digitalWrite(led1, LOW);
+delay(250);
+
+digitalWrite(led1, HIGH);
+delay(250);
+digitalWrite(led1, LOW);
+delay(250);
+
+digitalWrite(led1, HIGH);
+delay(250);
+digitalWrite(led1, LOW);
+delay(250);
+
+//I - short short
+digitalWrite(led1, HIGH);
+delay(250);
+digitalWrite(led1, LOW);
+delay(250);
+
+digitalWrite(led1, HIGH);
+delay(250);
+digitalWrite(led1, LOW);
+delay(250);
+
+//A - short long
+digitalWrite(led1, HIGH);
+delay(250);
+digitalWrite(led1, LOW);
+delay(250);
+
+digitalWrite(led1, HIGH);
+delay(500);
+digitalWrite(led1, LOW);
+delay(250);
+
+//M - long long
+digitalWrite(led1, HIGH);
+delay(500);
+digitalWrite(led1, LOW);
+delay(250);
+
+digitalWrite(led1, HIGH);
+delay(500);
+digitalWrite(led1, LOW);
+delay(5000);
+}
